200110bfa64509c39aa6919e2f4e305e479a02e9
jtrznadel <trznadeljakub3ai@gmail.com>
2024-04-02 16:49:34 +0200

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Jakub Trznadel on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
