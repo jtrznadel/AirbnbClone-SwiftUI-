b6bcaad3791bdd8fd1000d0a4264219d619fe58a
jtrznadel <trznadeljakub3ai@gmail.com>
2024-04-03 19:14:44 +0200

    expand listing's details

---
+++ AirbnbClone/Core/Components/ListingFeatureStack.swift
@@ -0,0 +1,18 @@
+//
+//  ListingFeatureStack.swift
+//  AirbnbClone
+//
+//  Created by Jakub Trznadel on 03/04/2024.
+//
+
+import SwiftUI
+
+struct ListingFeatureStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingFeatureStack()
+}
---
+++ AirbnbClone/Core/Listings/ListingDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingDetailsView.swift
+//  AirbnbClone
+//
+//  Created by Jakub Trznadel on 02/04/2024.
+//
+
+import SwiftUI
+
+struct ListingDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingDetailsView()
+}
